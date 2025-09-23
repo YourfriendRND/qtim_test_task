a2eb59cf1e846f69fb3d567fcbabc625e3c09781
YourfriendRND <kupeirc@gmail.com>
2025-09-23 08:38:08 +0400

    application environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import commonConfig from './config/common.config';
+import dbConfig from './config/db.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfig, commonConfig],
+      envFilePath: './.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.databaseUsername'),
+        password: configService.get('database.password'),
+        database: configService.get('database.database'),
+        autoLoadEntities: false,
+        migrationsRun: false,
+        synchronize: false,
+        logging: false,
+        entities: [join(__dirname, '/../**/*.entity{.ts,.js}')],
+        migrations: [join(__dirname, '/../src/migrations/*{.ts,.js}')],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [ConfigService],
 })
 export class AppModule {}
